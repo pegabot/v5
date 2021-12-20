ddb57b70a1600e46214d946bde6d4f8447a249ae
Gruselhaus <nico@gruselhaus.com>
2021-12-20 14:53:38 +0100

    added winston for logging

--- src/discord/bot.ts
+++ src/discord/bot.ts
@@ -5,6 +5,7 @@
  */
 
 import { Client, Intents } from "discord.js";
+import { createLogger, format, transports } from "winston";
 import { EventHandler } from "./handlers/eventHandler";
 import { InteractionHandler } from "./handlers/interactionHandler";
 import { ProcessEventHandler } from "./handlers/processEventHandler";
@@ -19,8 +20,11 @@ export class Bot {
   interactionHandler = new InteractionHandler(this);
   processEventHandler = new ProcessEventHandler(this);
 
+  logger = createLogger();
+
   constructor() {
     this.processEventHandler.setupEvents();
+    this.logger.add(new transports.Console({ format: format.combine(format.errors({ stack: true }), format.splat(), format.colorize(), format.simple()) }));
   }
 
   login() {
