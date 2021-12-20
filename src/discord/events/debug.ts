7be4251aa70823ca7ed1cc1f91d29e2653a361d1
Gruselhaus <nico@gruselhaus.com>
2021-12-20 23:37:11 +0100

    adding the debug, error and warn event handlers

---
+++ src/discord/events/debug.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021 Nico Finkernagel
+ * This code is licensed under the MIT license
+ * (see https://github.com/gruselhaus/studip-people-searcher/blob/main/LICENSE.md for details)
+ */
+
+import { bot } from "../../main";
+
+bot.eventHandler.register("debug", (message) => bot.logger.debug(message));
---
+++ src/discord/events/warn.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021 Nico Finkernagel
+ * This code is licensed under the MIT license
+ * (see https://github.com/gruselhaus/studip-people-searcher/blob/main/LICENSE.md for details)
+ */
+
+import { bot } from "../../main";
+
+bot.eventHandler.register("warn", (message) => bot.logger.warn(message));
