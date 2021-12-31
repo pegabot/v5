e1f1e6409c9aec6ef38c4a7b708a2ddafd5c9131
Gruselhaus <nico@gruselhaus.com>
2021-12-31 16:48:28 +0100

    Updating the Copyright notices. Hello 2022

--- scripts/i18n/action.sh
+++ scripts/i18n/action.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 The Pegabot authors
+# Copyright (c) 2021 - 2022 The Pegabot authors
 # This code is licensed under GNU Affero General Public License v3.0
 # (see https://github.com/pegabot/v5/blob/main/LICENSE for details)
  
